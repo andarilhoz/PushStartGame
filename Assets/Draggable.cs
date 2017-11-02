4f09fc1b790970a2d9d43b8325ea24553be06118
Magno Gouveia <owlflee@gmail.com>
2017-11-02 18:34:59 -0200

    Adiciona elementos arrastaveis na UI

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -3,9 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Draggable : MonoBehaviour {
-	
+
+	private GameObject drag = null;
+	void OnMouseDown()
+	{
+		if(drag == null){
+			drag = CreateDragObject();
+			Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			MoveDragToCursor();
+		}
+	}
 	void OnMouseDrag()
 	{
-		Debug.Log("Dragging: " + this.name);	
+		MoveDragToCursor();
+	}
+
+	void OnMouseUp()
+	{
+		Object.Destroy(drag);		
+	}
+
+	GameObject CreateDragObject(){
+		GameObject dragable = new GameObject("Drag");
+		dragable.AddComponent<SpriteRenderer>();
+		SpriteRenderer render = dragable.GetComponent<SpriteRenderer>();
+		SpriteRenderer myRender = transform.GetComponent<SpriteRenderer>();
+
+		render.sprite = myRender.sprite;
+		dragable.transform.localScale = transform.lossyScale;
+		return dragable;
+	}
+
+	void MoveDragToCursor(){
+		Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		drag.transform.position = pos;
 	}
 }
