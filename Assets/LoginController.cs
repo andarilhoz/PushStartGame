30154f8d0539c6a02255705588e1e54fc91cc6c5
Magno Gouveia <owlflee@gmail.com>
2017-11-02 15:37:47 -0200

    First Commit

---
+++ Assets/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginController : MonoBehaviour {
+
+	public InputField username;
+	public InputField password;
+
+	public void Login(){
+		Debug.Log(username);
+		Debug.Log(password);
+	}
+}
