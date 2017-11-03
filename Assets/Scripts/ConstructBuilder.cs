e3df154592ae3ece3b380c32d7eab89ee416ad3a
Magno Gouveia <owlflee@gmail.com>
2017-11-03 21:22:14 -0200

    Fixa construcoes no mapa

---
+++ Assets/Scripts/ConstructBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructBuilder : MonoBehaviour {
+
+
+	void OnMouseUp()
+	{
+		ConstructBuilding();
+	}
+
+	void ConstructBuilding(){
+		GameObject building = new GameObject(transform.name + " constructed");
+		building.AddComponent<SpriteRenderer>();
+		SpriteRenderer render = building.GetComponent<SpriteRenderer>();
+		SpriteRenderer myRender = transform.GetComponent<SpriteRenderer>();
+
+		
+
+		Vector3 buildingPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		buildingPos.z = 0;
+
+		building.transform.position = buildingPos;
+		render.sprite = myRender.sprite;
+		building.transform.localScale = transform.lossyScale;
+	}
+}
